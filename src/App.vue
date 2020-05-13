2b4f76b177fdcbaa5346244db075bc99af6c63d4
wanglei <1900452581@qq.com>
2020-05-13 15:26:37 +0800

    加入uploader组件 by 王雷

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="bsn">
+      <!-- <uploader :mode="'view'" bsnType="43"></uploader> -->
+      <uploader-file post-action="http://localhost:8000/upload" @input="handleFiles" :on-success="onSuccess" :on-start="onStart" :on-upload="onUpload" :on-delete="onDelete" :on-error="onError" :on-progress="onProgress" multiple>&#x4E0A;&#x4F20;&#x6587;&#x4EF6;</uploader-file>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import uploaderFile from './components/UploaderFile.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    uploaderFile,
   },
 };
 </script>
